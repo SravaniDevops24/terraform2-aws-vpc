fea63e1d4c0b3267f3a36ef5abba968743320241
SravaniDevops24 <sravanimaddula542@gmail.com>
2024-07-18 17:04:31 +0530

    terraform-aws-vpc

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,10 @@ output "database_subnet_group_ids" {
   value = aws_db_subnet_group.default.id
 }
 
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
 output "igw_id" {
   value = aws_internet_gateway.gw.id
 }
